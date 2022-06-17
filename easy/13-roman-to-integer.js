6400aa7ddb51b2793661d6ae11ea84aaa091f214
sugampradhanpvt <107707070+sugampradhanpvt@users.noreply.github.com>
2022-06-17 22:37:14 +0530

    Update 13-roman-to-integer.js

--- easy/13-roman-to-integer.js
+++ easy/13-roman-to-integer.js
@@ -1,1 +1,29 @@
 // Roman to Integer
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var romanValueList = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+};
+
+var convertedIP = s.split('');
+var sum = 0;
+for(let i = 0; i < convertedIP.length -1 ; i++) {
+    if(romanValueList[convertedIP[i]] < romanValueList[convertedIP[i+1]]){
+        // if true then minus
+        sum -= romanValueList[convertedIP[i]];
+    } else {
+        sum += romanValueList[convertedIP[i]];
+    }
+}
+    sum += romanValueList[convertedIP[convertedIP.length-1]];
+    return sum;
+}
